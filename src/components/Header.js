e30983cd968ac3a311bff1629d75929d840c32ce
vishwajeet14all <rjvishwajeet007@gmail.com>
2023-09-16 22:38:39 +0530

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+export default function Header() {
+  return (
+    <header className="header">
+      <img className="header--image" src="../images/troll-face.png" alt="troll_face" />
+      <h1 className="header--title">Meme Generator</h1>
+      <h3 className="header--project">React - Project 3</h3>
+    </header>
+  );
+}
